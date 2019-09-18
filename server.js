d50909bb0f207d7490eac52dbe38483d4a238d53
Nicholas Leone <nick.al.leone@gmail.com>
2019-09-18 13:57:27 -0700

    Removing controllers folder and adding route logic

--- server.js
+++ server.js
@@ -1,24 +1,25 @@
 const express = require("express");
 const mongoose = require("mongoose");
-// const routes = require("./routes");
+const routes = require("./routes");
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8000;
 
 // Configure body parsing for AJAX requests
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Serve up static assets
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+// }
 
 // Add routes, both API and view
-// app.use(routes);
+app.use(routes);
+
 
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://user1:password1@ds125871.mlab.com:25871/heroku_0xn0jnk7",
+  process.env.MONGODB_URI || "mongodb://localhost/quizness",
   {
     useCreateIndex: true,
     useNewUrlParser: true

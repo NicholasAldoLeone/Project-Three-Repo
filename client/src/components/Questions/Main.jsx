81edc6ab9012ada8fcf2f9b7ac5e6213cfdb7566
Connor Dougherty <connor.j.dougherty9@gmail.com>
2019-09-19 11:30:48 -0700

    Colors working

--- client/src/components/Questions/Main.jsx
+++ client/src/components/Questions/Main.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import data from '../../data/data';
 import Answers from '../Answers/Answers';
-
 class Main extends React.Component {
     constructor(props) {
         super(props);
@@ -12,38 +11,78 @@ class Main extends React.Component {
             showButton: false,
             questionAnswered: false,
             score: 0,
+            classNames: ['', '', '', '']
         }
         this.nextQuestion = this.nextQuestion.bind(this);
         this.handleShowButton = this.handleShowButton.bind(this);
         this.handleStartQuiz = this.handleStartQuiz.bind(this);
+        this.resetClasses = this.resetClasses.bind(this)
         this.handleIncreaseScore = this.handleIncreaseScore.bind(this);
+        this.checkAnswer.bind(this)
     }
-
     pushData(nr) {
         this.setState({
             question: data[nr].question,
-            answers: [data[nr].answers[0], data[nr].answers[1], data[nr].answers[2], data[nr].answers[3] ],
+            answers: [data[nr].answers[0], data[nr].answers[1], data[nr].answers[2], data[nr].answers[3]],
             correct: data[nr].correct,
             nr: this.state.nr + 1
         });
     }
-
+    resetClasses = () => {
+        this.setState({
+            classNames: ['', '', '', '']
+        })
+    }
     componentWillMount() {
         let { nr } = this.state;
         this.pushData(nr);
     }
-
     nextQuestion() {
         let { nr, total, score } = this.state;
-            this.pushData(nr);
+        this.pushData(nr);
+        this.setState({
+            showButton: false,
+            questionAnswered: false,
+        });
+        console.log(this.state.questionAnswered)
+        this.resetClasses()
+    }
+
+    checkAnswer = (e) => {
+
+
+        if (!this.state.questionAnswered) {
+            let elem = e.target;
+            let correct = parseInt(this.state.correct);
+            console.log(correct)
+            let answer = parseInt(elem.dataset.id);
+            console.log(answer)
+            let updatedClassNames = this.state.classNames;
+
+            if (answer === correct) {
+                updatedClassNames[answer - 1] = 'right';
+                console.log("Correct!")
+                // increaseScore();
+            }
+            else {
+                console.log("test")
+                updatedClassNames[answer - 1] = 'wrong';
+            }
+
             this.setState({
-                showButton: false,
-                questionAnswered: true
-            });
-            console.log(this.state.questionAnswered)
+                classNames: updatedClassNames
+            })
+
+            this.handleShowButton();
+
         }
 
-    
+        // if (isAnswered) {
+        //     this.setState({
+        //         classNames: ['', '', '', '']
+        //     });
+        // }
+    }
 
     handleShowButton() {
         this.setState({
@@ -65,7 +104,7 @@ class Main extends React.Component {
     }
 
     render() {
-        let { nr, total, question, answers, correct, showButton, questionAnswered, score} = this.state;
+        let { nr, total, question, answers, correct, showButton, questionAnswered, score, classNames } = this.state;
 
         return (
             <div className="container">
@@ -76,9 +115,9 @@ class Main extends React.Component {
                             <h4>Question {nr}</h4>
                             <p>{question}</p>
                         </div>
-                        <Answers answers={answers} correct={correct} showButton={this.handleShowButton} isAnswered={questionAnswered} increaseScore={this.handleIncreaseScore}/>
+                        <Answers answers={answers} correct={correct} classes={classNames} checkAnswer={this.checkAnswer} increaseScore={this.handleIncreaseScore} />
                         <div id="submit">
-                            {showButton ? <button className="fancy-btn" onClick={this.nextQuestion} >{nr===total ? 'Finish quiz' : 'Next question'}</button> : null}
+                            {showButton ? <button className="fancy-btn" onClick={this.nextQuestion} >{nr === total ? 'Finish quiz' : 'Next question'}</button> : null}
                         </div>
                     </div>
                 </div>

2d789af8dd3135be44321f48a4004de38f9d4127
TashaMac88 <latashammiller@gmail.com>
2019-09-23 13:23:14 -0700

    good

--- client/src/components/Form/form.js
+++ client/src/components/Form/form.js
@@ -1,56 +1,22 @@
 import React from "react";
-// import ReactDOM from "react-dom";
 import 'react-bootstrap';
 import { Jumbotron, Container, Form, Button } from 'react-bootstrap';
 import "../Form/form.css";
 
 class NewForm extends React.Component {
     //build req.body for us, send information across to routes to put inside database
-    createQuiz = () => { 
-        
-   }
-    render() {
+render() {
         return (
             <div>
-                <Jumbotron fluid>
-                    <div class="header">
+               <Jumbotron fluid>
                     <Container>
                         <h1 class="tron">Create Your Quiz!</h1>
                     </Container>
-                    </div>
                 </Jumbotron>
 
                 <Form className="col-sm-6 container-fluid myform">
                     <Form.Group controlId="Title">
                         <Form.Label>Title of Your Quiz</Form.Label>
-                        <Form.Control type="question" id = "1" />
-                    </Form.Group>
-                    <Form.Group controlId="Question">
-                        <Form.Label>Question</Form.Label>
-                        <Form.Control type="question" />
-                    </Form.Group>
-                    <Form.Group controlId="Answer">
-                        <Form.Label>Question</Form.Label>
-                        <Form.Control type="answer" />
-                    </Form.Group>
-                    <Form.Group controlId="Answer">
-                        <Form.Label>Question</Form.Label>
-                        <Form.Control type="answer" />
-                    </Form.Group>
-                    <Form.Group controlId="Answer">
-                        <Form.Label>Question</Form.Label>
-                        <Form.Control type="answer" />
-                    </Form.Group>
-                    <Form.Group controlId="Answer">
-                        <Form.Label>Answer</Form.Label>
-                        <Form.Control type="answer" />
-                    </Form.Group>
-                    
-
-
-
-                <Form className="col-sm-6 container-fluid myform">
-                    <Form.Group controlId="Title">
                         <Form.Control type="question" />
                     </Form.Group>
                     <Form.Group controlId="Question">
@@ -71,6 +37,10 @@ class NewForm extends React.Component {
                     </Form.Group>
                     <Form.Group controlId="Answer">
                         <Form.Label>Answer</Form.Label>
+                        <Form.Control type="answer" />
+                    </Form.Group>
+                    <Form.Group controlId="Correct Answer">
+                        <Form.Lable>Answer</Form.Lable>
                         <Form.Control type="answer" />
                     </Form.Group>
                 </Form>
@@ -78,9 +48,6 @@ class NewForm extends React.Component {
 
 
                 <Form className="col-sm-6 container-fluid myform">
-                    <Form.Group controlId="Title">
-                        <Form.Control type="question" />
-                    </Form.Group>
                     <Form.Group controlId="Question">
                         <Form.Label>Question</Form.Label>
                         <Form.Control type="question" />
@@ -101,10 +68,38 @@ class NewForm extends React.Component {
                         <Form.Label>Answer</Form.Label>
                         <Form.Control type="answer" />
                     </Form.Group>
-                    <Button variant="primary" type="submit" onClick = {()=> this.createQuiz () } >
-                        Submit
-                    </Button>
+                    <Form.Group controlId="Correct Answer">
+                        <Form.Contol type="Correct Answer" />
+                    </Form.Group>
                 </Form>
+
+
+
+                <Form className="col-sm-6 container-fluid myform">
+                    <Form.Group controlId="Question">
+                        <Form.Label>Question</Form.Label>
+                        <Form.Control type="question" />
+                    </Form.Group>
+                    <Form.Group controlId="Answer">
+                        <Form.Label>Answer</Form.Label>
+                        <Form.Control type="answer" />
+                    </Form.Group>
+                    <Form.Group controlId="Answer">
+                        <Form.Label>Answer</Form.Label>
+                        <Form.Control type="answer" />
+                    </Form.Group>
+                    <Form.Group controlId="Answer">
+                        <Form.Label>Answer</Form.Label>
+                        <Form.Control type="answer" />
+                    </Form.Group>
+                    <Form.Group controlId="Answer">
+                        <Form.Label>Answer</Form.Label>
+                        <Form.Control type="answer" />
+                    </Form.Group>
+                    <Form.Group controlId="Correct Answer">
+                        <Form.Label>Correct Answer</Form.Label>
+                        <Form.Control type="Correct Answer" />
+                    </Form.Group>
                 </Form>
             </div>
         )

774486445793187ef575fbc006e31817f348dc88
Connor Dougherty <connor.j.dougherty9@gmail.com>
2019-09-25 11:52:10 -0700

    Updated Home page instructions

--- client/src/App.js
+++ client/src/App.js
@@ -5,21 +5,31 @@ import Main from './components/Questions/Main';
 import './styles/quiztake.css';
 import Quizzes from "./pages/Quizzes.js"
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
-
+import Carousel from "./components/HomeCarousel"
+import { Container, Col, Row } from './components/Grid'
 function App() {
   return (
     <div>
-    <Router>
+      <Router>
         <div>
           <Navbar />
+          <div>
+
+
+            <Carousel />
+
+
+
+          </div>
           <Switch>
-            <Route exact path="/" component={Quizzes} />
-            <Route exact path="/quiz/:id" component={Main} />
+            <Route exact path="/" component={Quizzes} label="Insert Quiz ID" />
+            <Route exact path="/quiz/:id" component={Main} label="Insert Quiz ID" />
             <Route exact path="/create" component={CreateForm} />
 
           </Switch>
+
         </div>
-  </Router>
+      </Router>
     </div>
   )
 };
